72a0951295358c06d756b8ab5a798ae336ae5985
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-13 13:42:47 +0200

    Renamed func

--- implementation.go
+++ implementation.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // TODO: document this function.
 // PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
+func PrefixToInfix(input string) (string, error) {
 	return "TODO", fmt.Errorf("TODO")
 }
--- implementation_test.go
+++ implementation_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPrefixToInfix(t *testing.T) {
+	res, err := PrefixToInfix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "4 2 - 3 * 5 +", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExamplePrefixToInfix() {
+	res, _ := PrefixToInfix("+ 2 2")
 	fmt.Println(res)
 
 	// Output:
