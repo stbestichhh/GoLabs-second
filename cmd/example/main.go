9d92988a152eafa41cd95e563a44e8f48fe2d7e5
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-18 16:57:15 +0200

    Check input

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,16 +4,38 @@ import (
 	"flag"
 	"fmt"
 	"go_lab_second"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	inputfile = flag.String("f", "", "Path/to/expression-file")
+	inputFile = flag.String("f", "", "Path/to/expression-file")
 	outputFile = flag.String("o", "", "Path/to/output-file")
 )
 
 func main() {
 	flag.Parse()
+
+	var input io.Reader
+	var output io.Writer
+
+	if *inputExpression != "" {
+		input = strings.NewReader(*inputExpression)
+	} else if *inputFile != "" {
+		file, err := os.Open(*inputFile)
+
+		if err != nil {
+			fmt.Println(os.Stderr, "Cannot open file.", err)
+		}
+
+		defer file.Close()
+
+		input = file;
+	} else {
+		fmt.Println(os.Stderr, "Usage: go run cmd/example/main.go -e <expression> | -f <path/to/expression-file [-o <path/to/outputfile>]")
+	}
 
 	// TODO: Change this to accept input from the command line arguments as described in the task and
 	//       output the results using the ComputeHandler instance.
