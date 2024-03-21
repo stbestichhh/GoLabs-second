93c2e84c57522a8abd619f105a165581eabeeac4
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2024-03-21 20:24:34 +0200

    Prefix to Infix

--- implementation.go
+++ implementation.go
@@ -1,7 +1,46 @@
-package lab2
+package main
 
-type PrefixCalculator struct{}
+import (
+	"fmt"
+)
 
-func (ptic *PrefixCalculator) ConvertPrefixToInfix(expression string) (int, error) {
-	return 0, nil
+func isOperator(x rune) bool {
+	switch x {
+	case '+', '-', '*', '/', '^', '%':
+		return true
+	}
+	return false
+}
+
+func convert(str string) string {
+	stack := []string{}
+
+	l := len(str)
+
+	for i := l - 1; i >= 0; i-- {
+		c := rune(str[i])
+
+		if isOperator(c) {
+			if len(stack) >= 2 {
+				op1 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				op2 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				temp := fmt.Sprintf("(%s%c%s)", op1, c, op2)
+				stack = append(stack, temp)
+			} else {
+				return "Invalid prefix expression"
+			}
+		} else {
+			stack = append(stack, string(c))
+		}
+	}
+	return stack[len(stack)-1]
+}
+
+func main() {
+	exp := "your math problem here"
+
+	fmt.Println("Infix :", convert(exp))
 }
