9a2873109ff5ad713d5012cc54678592908c37e7
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-21 16:31:29 +0200

    hndler_test.go

---
+++ handler_test.go
@@ -0,0 +1,48 @@
+package lab2
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompute(t *testing.T) {
+	type fields struct {
+		Input  io.Reader
+		Output io.Writer
+	}
+
+	var writer bytes.Buffer
+	mockError := "mockError"
+
+	tests := []struct {
+		name       string
+		fields     fields
+		calculator PrefixCalculatorSpy
+		isError    bool
+	}{
+		{"Should pass", fields{strings.NewReader("+ 5 * - 4 2 3"), &writer}, PrefixCalculatorSpy{+5, nil}, false},
+		{"Should pass", fields{strings.NewReader("5 -"), &writer}, PrefixCalculatorSpy{0, errors.New(mockError)}, true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			computeHandler := &ComputeHandler{
+				Input:      test.fields.Input,
+				Output:     test.fields.Output,
+				Calculator: &test.calculator,
+			}
+			err := computeHandler.Compute()
+
+			if test.isError {
+				assert.Error(t, err, mockError)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
--- implementation.go
+++ implementation.go
@@ -2,8 +2,6 @@ package lab2
 
 type PrefixCalculator struct{}
 
-// TODO: document this function.
-// PrefixToPostfix converts
 func (ptic *PrefixCalculator) ConvertPrefixToInfix(expression string) (int, error) {
 	return 0, nil
 }
