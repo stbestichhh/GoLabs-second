9dae71116c9362ef0508badbb1962aaecd01ce72
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-03-21 23:18:09 +0300

    Test for function ConvertPrefixToInfix

--- implementation_test.go
+++ implementation_test.go
@@ -1,11 +1,53 @@
 package lab2
 
 import (
-	// "fmt"
 	"testing"
-	// "github.com/stretchr/testify/assert"
+	//lab2 "go_lab_second"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestConvertPrefixToInfix(t *testing.T) {}
+func TestConvertPrefixToInfix(t *testing.T) {
+	tests := []struct {
+		name       string
+		expression string
+		expected   string
+		err        error
+	}{
+		{
+			name:       "Simple expression",
+			expression: "+ 2 3",
+			expected:   "2 + 3",
+			err:        nil,
+		},
+		{
+			name:       "Complex expression",
+			expression: "+ * 2 3 4",
+			expected:   "2 * 3 + 4",
+			err:        nil,
+		},
+		{
+			name:       "Empty expression",
+			expression: "",
+			expected:   "",
+			err:        errors.New("empty expression"),
+		},
+		{
+			name:       "Invalid expression",
+			expression: "abc",
+			expected:   "",
+			err:        errors.New("invalid expression"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			calc := &lab2.PrefixCalculatorSpy{Result: tt.expected, Error: tt.err}
+			result, err := calc.ConvertPrefixToInfix(tt.expression)
+
+			assert.Equal(t, tt.expected, result)
+			assert.Equal(t, tt.err, err)
+		})
+	}
+}
 
 func ExampleConvertPrefixToInfix() {}
